5d0f2965fe6711be70a8185c58bc1ff420e15d56
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-10 00:58:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(roster)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(pioneer_calls)
+  pioneer_calls.map! {|call| call.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
